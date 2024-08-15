52236f60117f8c3e7383b4b796ef7730dde4a951
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-08-15 18:28:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 function firstWord(s) {
   // your code here
+	let firstWordAns = "";
+	let i=0;
+	let flag = true;
+	while (flag === true || i <= s.length) {
+		let char = s[i];
+		if(char === " "){
+			flag = false;
+			return firstWordAns;
+		}
+		else{
+			firstWordAns = `${firstWordAns}${char}`;
+			i++;
+		}
+	}
+	return "";
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+// const s = prompt("Enter String:");
 alert(firstWord(s));
